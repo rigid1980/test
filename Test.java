cae275c0016c12b78bc7e92a70171e5e05bbea8f
Berndt Meng <mengbin@tsinghua.org.cn>
2022-12-15 18:10:03 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,12 +8,30 @@ public class Test {
         // Write your code here
         List<String> numbers = new ArrayList<String>();
         String line = "每月2次(週2/4) 2 Times Per Month (Week 2 / 4)";
-        Pattern p = Pattern.compile("週\\d+");
-        Matcher m = p.matcher(line); 
-        while (m.find()) {
-            //numbers.add(m.group());
-            int n = Integer.parseInt(m.group());
-            System.out.println("週  "+n);
-        }
+        // Pattern p = Pattern.compile("週\\d+");
+        // Matcher m = p.matcher(line); 
+        // while (m.find()) {
+        //     //numbers.add(m.group());
+        //     int n = Integer.parseInt(m.group());
+        //     System.out.println("週  "+n);
+        // }
+
+        int iw = line.indexOf("週");
+        try {
+            String subline = line.substring(iw+1);
+            int Week = Integer.parseInt(subline.substring(0,1));
+            System.out.println("週  "+Week);
+
+            iw = line.indexOf("/");
+            if(iw !=-1){
+                subline = line.substring(iw+1);
+                Week = Integer.parseInt(subline.substring(0,1));
+                System.out.println("週  "+Week);
+            }
+            
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }		
+
     }
 }
