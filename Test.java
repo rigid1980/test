edb8973ee0ddce50eb76f59cb440d797ee223b62
Berndt Meng <mengbin@tsinghua.org.cn>
2022-12-20 20:17:45 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,7 +7,11 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
 import java.time.DayOfWeek;
+import static java.time.temporal.TemporalAdjusters.firstInMonth;
+
 
 public class Test {
     public static void main(String[] args) {
@@ -18,20 +22,61 @@ public class Test {
         System.out.println("週  "+week[6].getValue());
         System.out.println("週  "+week[1].getValue());
         
-        LocalDate ret = test.getDateByWeekandDay(LocalDate.now(), 3, week[0]);
+        LocalDate ret = test.getDateByWeekandDay(LocalDate.now(), 2, week[0]);
         System.out.println("day  "+ret);
 
-         ret = test.getDateByWeekandDay(LocalDate.now(), 3, week[1]);
+         ret = test.getDateByWeekandDay(LocalDate.now(), 5, week[1]);
         System.out.println("day  "+ret);
+
+        // create WeekFields
+        WeekFields weekFields
+            = WeekFields.of(DayOfWeek.MONDAY, 1);
+  
+        // apply dayOfWeek()
+        TemporalField dayOfWeek
+            = weekFields.dayOfWeek();
+  
+        // create a LocalDate
+        LocalDate day = LocalDate.of(2022, 12, 12);
+  
+        // get day of week for localdate
+        int dow = day.get(dayOfWeek);
+  
+        // print results
+        System.out.println("day of week for "
+                           + day + " :" + dow);
+
+        LocalDate now = LocalDate.now(); //2015-11-23
+        LocalDate firstMonday = now.with(firstInMonth(DayOfWeek.MONDAY)); 
+
+        System.out.println("1st Mondy for "
+                           + now + " :" + firstMonday);
+
+        System.out.println("week  "      + getWeek(day));
         
+    }
+
+    static LocalDate firstMonday = LocalDate.now().with(firstInMonth(DayOfWeek.MONDAY)); 
+
+    static int  getWeek(LocalDate sDate){
+        int week = 0;
+        LocalDate monday = firstMonday;
+        while(!sDate.isBefore(monday)){
+            monday = monday.plusDays(7);
+            week++;
+            System.out.println( " sfsf monday "      + monday);
+            System.out.println( " sfsf week  "      + week);
+        }
+        return week;
     }
 
     LocalDate getDateByWeekandDay(LocalDate startDate, int week, DayOfWeek dayOfWeek){
 		LocalDate weekDate = startDate
-					.with(TemporalAdjusters.firstDayOfMonth())
-					.with(TemporalAdjusters.nextOrSame(dayOfWeek))
+					//.with(TemporalAdjusters.firstDayOfMonth())
+					.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY))
 					.plusDays(7*(week-1));
-		return weekDate;
+		if(weekDate.getMonth() == startDate.getMonth()) return weekDate;
+		return null;
 	}
 
     void testCalDate(){
