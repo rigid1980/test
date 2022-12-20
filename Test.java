dc605e3efe2265a055d42c7be08d712998a292ee
Berndt Meng <mengbin@tsinghua.org.cn>
2022-12-20 17:38:19 +0800

    date

--- Test.java
+++ Test.java
@@ -3,34 +3,65 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.time.DayOfWeek;
+
 public class Test {
     public static void main(String[] args) {
-        // Write your code here
-        List<String> numbers = new ArrayList<String>();
-        String line = "每月2次(週2/4) 2 Times Per Month (Week 2 / 4)";
-        // Pattern p = Pattern.compile("週\\d+");
-        // Matcher m = p.matcher(line); 
-        // while (m.find()) {
-        //     //numbers.add(m.group());
-        //     int n = Integer.parseInt(m.group());
-        //     System.out.println("週  "+n);
-        // }
+        Test test= new Test();
 
-        int iw = line.indexOf("週");
-        try {
-            String subline = line.substring(iw+1);
-            int Week = Integer.parseInt(subline.substring(0,1));
-            System.out.println("週  "+Week);
+        DayOfWeek week[] =  DayOfWeek.values();
+        System.out.println("週  "+week[0]);
+        System.out.println("週  "+week[6].getValue());
+        System.out.println("週  "+week[1].getValue());
+        
+        LocalDate ret = test.getDateByWeekandDay(LocalDate.now(), 3, week[0]);
+        System.out.println("day  "+ret);
 
-            iw = line.indexOf("/");
-            if(iw !=-1){
-                subline = line.substring(iw+1);
-                Week = Integer.parseInt(subline.substring(0,1));
-                System.out.println("週  "+Week);
-            }
-            
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }		
+         ret = test.getDateByWeekandDay(LocalDate.now(), 3, week[1]);
+        System.out.println("day  "+ret);
+        
     }
+
+    LocalDate getDateByWeekandDay(LocalDate startDate, int week, DayOfWeek dayOfWeek){
+		LocalDate weekDate = startDate
+					.with(TemporalAdjusters.firstDayOfMonth())
+					.with(TemporalAdjusters.nextOrSame(dayOfWeek))
+					.plusDays(7*(week-1));
+		return weekDate;
+	}
+
+    void testCalDate(){
+         // Write your code here
+         List<String> numbers = new ArrayList<String>();
+         String line = "每月2次(週2/4) 2 Times Per Month (Week 2 / 4)";
+         // Pattern p = Pattern.compile("週\\d+");
+         // Matcher m = p.matcher(line); 
+         // while (m.find()) {
+         //     //numbers.add(m.group());
+         //     int n = Integer.parseInt(m.group());
+         //     System.out.println("週  "+n);
+         // }
+ 
+         int iw = line.indexOf("週");
+         try {
+             String subline = line.substring(iw+1);
+             int Week = Integer.parseInt(subline.substring(0,1));
+             System.out.println("週  "+Week);
+ 
+             iw = line.indexOf("/");
+             if(iw !=-1){
+                 subline = line.substring(iw+1);
+                 Week = Integer.parseInt(subline.substring(0,1));
+                 System.out.println("週  "+Week);
+             }
+             
+         } catch (NumberFormatException e) {
+             e.printStackTrace();
+         }		
+    }
+
 }
